a8881fedb2c38792ad52f07b1deda7e879ea3fbf
kdr250 <ytkodera4863@gmail.com>
2019-09-02 11:35:01 +0900

    Add-Test-Code
    ・コントローラーのテストコードを追加しました。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,10 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 
+  RSpec.configure do |config|
+    Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+    config.include Devise::Test::ControllerHelpers, type: :controller
+    config.include ControllerMacros, type: :controller
+  end
+
 end
