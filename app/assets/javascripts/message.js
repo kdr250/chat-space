1d65a68f7a48e40a254ba9c1bbc75d22fa95f895
kdr250 <ytkodera4863@gmail.com>
2019-09-03 17:42:21 +0900

    Add-Ajax-in-Message-Feature-v2
    ・javascriptの三項演算子を導入し、html生成分岐処理のコードを簡略化しました。
    ・フォームの初期化にはvalの代わりにresetを導入しました。
    ・console.logを削除しました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 
 $(function(){
-  function buildHTML(message){
+  function buildHTML(message, text, image){
     var html = `<div class="right__contents__messagebox">
     <div class="right__contents__messagebox__message--name">
     ${message.user_name}
@@ -9,42 +9,15 @@ $(function(){
     ${message.created_at}
     </div>
     <div class="right__contents__messagebox--text">
-    ${message.text}
-    <br>
-    
-    </div>
-    <img alt="test" style="margin-top: 10px;" src=${message.image.url} width="200" height="132">
-    </div>`
-    return html;
-  }
+        ${text}
+        </div>`;
 
-  function buildHTML_NoImg(message){
-    var html = `<div class="right__contents__messagebox">
-    <div class="right__contents__messagebox__message--name">
-    ${message.user_name}
-    </div>
-    <div class="right__contents__messagebox__message--date">
-    ${message.created_at}
-    </div>
-    <div class="right__contents__messagebox--text">
-    ${message.text}
-    <br>`
-    return html;
-  }
-
-  function buildHTML_NoText(message){
-    var html = `<div class="right__contents__messagebox">
-    <div class="right__contents__messagebox__message--name">
-    ${message.user_name}
-    </div>
-    <div class="right__contents__messagebox__message--date">
-    ${message.created_at}
-    </div>
-    <div class="right__contents__messagebox--text">
-    <br>
-    </div>
-    <img alt="test" style="margin-top: 10px;" src=${message.image.url} width="200" height="132">
-    </div>`
+  if (image != null) {
+    html += `
+          <div>
+          <img alt="test" style="margin-top: 10px;" src=${image} width="200" height="132">
+          </div>`;
+    };
     return html;
   }
 
@@ -53,7 +26,6 @@ $(function(){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $('#new_message').attr('action');
-    // console.log("hi")
     $.ajax({
       url: url,
       type: "POST",
@@ -63,19 +35,12 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      console.log(data.text)
-      console.log(data.image.url)
-      if (data.text == null) {
-        var html = buildHTML_NoText(data);
-      } else if (data.image.url == null) {
-        var html = buildHTML_NoImg(data);
-      } else {
-        var html = buildHTML(data);
-      }
+      var text = data.text ? data.text : '';
+      var image = data.image.url ? data.image.url : null;
+      var html = buildHTML(data, text, image);
 
       $('.right__contents').append(html);
-      $('#message_text').val('');
-      $('#message_image').val('');
+      $('#new_message').get(0).reset();
       //データ受け取り後画面最下部までスクロール
       $('.right__contents').animate({ scrollTop: $('.right__contents')[0].scrollHeight});
     })
