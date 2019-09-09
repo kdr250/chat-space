9759b28a0c59bcb088f836a8b5e35c6b6b518151
kdr250 <ytkodera4863@gmail.com>
2019-09-09 17:29:48 +0900

    Repair-Date-in-Rails-and-Ajax-ver2
    コメント行を取り除きました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,8 +4,6 @@ $(function(){
   function buildHTML(message){
     var text = message.text ? message.text : '';
     var image = message.image.url ? `<img alt="test" src="${message.image.url}" width="200" height="132" style="margin-top: 10px;" >` : '';
-    // var today = new Date();
-    // var created_at = today.getFullYear() + '/' + today.getMonth() + '/' + today.getDate() + ' ' + today.getHours() + ":" + today.getHours()
     
     var html = `<div class="right__contents__messagebox" data-id='${message.id}'>
       <div class="right__contents__messagebox__message--name">
